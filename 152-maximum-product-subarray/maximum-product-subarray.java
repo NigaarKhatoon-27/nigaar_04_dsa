746e129ea7ab6a1113be0a410898b5ee85b6d1ea
NIGAAR KHATOON <khatoonnigaar953@gmail.com>
2026-06-16 22:33:04 +0530

    Time: 1 ms (92.36%) | Memory: 47 MB (93.41%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.java
+++ 152-maximum-product-subarray/maximum-product-subarray.java
@@ -15,19 +15,35 @@ class Solution {
         // return maxi;
 
         //Better O(n^2)
-           if(nums.length==0) return 0;
-        int maxi = Integer.MIN_VALUE;
-        for(int i=0;i<nums.length;i++){
-             int prod = 1;
-            for(int j=i;j<nums.length;j++){
+        //    if(nums.length==0) return 0;
+        // int maxi = Integer.MIN_VALUE;
+        // for(int i=0;i<nums.length;i++){
+        //      int prod = 1;
+        //     for(int j=i;j<nums.length;j++){
                 
-                    prod = prod * nums[j];
-                    maxi = Math.max(prod , maxi);
+        //             prod = prod * nums[j];
+        //             maxi = Math.max(prod , maxi);
                 
-            }
-        }
-        return maxi;
+        //     }
+        // }
+        // return maxi;
         
+
+        //Optimal
+          if(nums.length==0) return 0;
+
+          int n = nums.length;
+         int maxi = Integer.MIN_VALUE;
+         int prefix = 1;
+         int suffix = 1;
+         for(int i=0;i<nums.length;i++){
+            if(prefix == 0) prefix = 1;
+            if(suffix == 0) suffix = 1;
+             prefix = prefix * nums[i];
+             suffix = suffix * nums[n-i-1];
+             maxi =  Math.max(maxi , Math.max(prefix , suffix));
+         }
+         return maxi;
 
     }
 }
