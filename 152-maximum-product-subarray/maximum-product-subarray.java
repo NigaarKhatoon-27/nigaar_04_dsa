48438284fb9db5182f1404bafa58999f36e209db
NIGAAR KHATOON <khatoonnigaar953@gmail.com>
2026-06-16 22:36:51 +0530

    Time: 0 ms (100.00%) | Memory: 47.1 MB (71.85%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.java
+++ 152-maximum-product-subarray/maximum-product-subarray.java
@@ -30,20 +30,43 @@ class Solution {
         
 
         //Optimal
-          if(nums.length==0) return 0;
+        //   if(nums.length==0) return 0;
 
-          int n = nums.length;
-         int maxi = Integer.MIN_VALUE;
-         int prefix = 1;
-         int suffix = 1;
-         for(int i=0;i<nums.length;i++){
-            if(prefix == 0) prefix = 1;
-            if(suffix == 0) suffix = 1;
-             prefix = prefix * nums[i];
-             suffix = suffix * nums[n-i-1];
-             maxi =  Math.max(maxi , Math.max(prefix , suffix));
-         }
-         return maxi;
+        //   int n = nums.length;
+        //  int maxi = Integer.MIN_VALUE;
+        //  int prefix = 1;
+        //  int suffix = 1;
+        //  for(int i=0;i<nums.length;i++){
+        //     if(prefix == 0) prefix = 1;
+        //     if(suffix == 0) suffix = 1;
+        //      prefix = prefix * nums[i];
+        //      suffix = suffix * nums[n-i-1];
+        //      maxi =  Math.max(maxi , Math.max(prefix , suffix));
+        //  }
+        //  return maxi;
+
+
+        //2nd Optimal
+        int n = nums.length;
+        int prod = 1;
+        int maxi =Integer.MIN_VALUE;
+        //left to right
+        for(int i=0;i<nums.length;i++){
+            prod = prod * nums[i];
+            maxi = Math.max(prod , maxi);
+
+            if(prod == 0) prod = 1; 
+        }
+        //right to left
+        prod = 1;
+          for(int i=n-1;i>=0;i--){
+            prod = prod * nums[i];
+            maxi = Math.max(prod , maxi);
+
+            if(prod == 0) prod = 1; 
+        }
+        return maxi;
+
 
     }
 }
