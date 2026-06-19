191035f6bd29d8708c898c201988ed9dda4af1d1
NIGAAR KHATOON <khatoonnigaar953@gmail.com>
2026-06-20 00:33:50 +0530

    Time: 29 ms (93.33%) | Memory: 59.1 MB (73.85%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -17,21 +17,55 @@ class Solution {
         // return new ArrayList<>(set);
 
         //Better
-        Set<List<Integer>> set = new HashSet<>();
+        // Set<List<Integer>> set = new HashSet<>();
+        // for(int i=0;i<nums.length;i++){
+        //     HashSet<Integer> hashset = new HashSet<>();
+        //     for(int j=i+1;j<nums.length;j++){
+        //         int third = - (nums[i] + nums[j]);
+        //         if(hashset.contains(third)){
+        //             List<Integer> lt = Arrays.asList(nums[i] , nums[j] , third);
+        //             Collections.sort(lt);
+        //             set.add(lt);
+        //         }
+        //         else{
+        //             hashset.add(nums[j]);
+        //         }
+        //     }
+        // }
+        // return new ArrayList<>(set);
+
+        //Optimal
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums);
+        int n = nums.length;
         for(int i=0;i<nums.length;i++){
-            HashSet<Integer> hashset = new HashSet<>();
-            for(int j=i+1;j<nums.length;j++){
-                int third = - (nums[i] + nums[j]);
-                if(hashset.contains(third)){
-                    List<Integer> lt = Arrays.asList(nums[i] , nums[j] , third);
-                    Collections.sort(lt);
-                    set.add(lt);
+            if(i>0 && nums[i] == nums[i-1]){
+                continue;
+            }
+            int left = i+1;
+            int right = n-1;
+            while(left < right){
+                int sum = nums[i]+nums[left] + nums[right];
+                if(sum < 0){
+                    left++;
+                }
+                else if(sum > 0){
+                    right--;
                 }
                 else{
-                    hashset.add(nums[j]);
+ans.add(Arrays.asList(nums[i] , nums[left], nums[right]));
+left++;
+right--;
+while(left < right && nums[left]== nums[left-1]){
+    left++;
+}
+while(left < right && nums[right]== nums[right+1]){
+    right--;
+}
                 }
             }
         }
-        return new ArrayList<>(set);
+        return ans;
+
     }
 }
