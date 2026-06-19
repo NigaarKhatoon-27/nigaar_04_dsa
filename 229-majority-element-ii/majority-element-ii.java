10b690c7c3d73d58f702fe1f7d966b1d8f68f7ab
NIGAAR KHATOON <khatoonnigaar953@gmail.com>
2026-06-19 10:25:09 +0530

    Time: 16 ms (21.57%) | Memory: 52.7 MB (28.10%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,22 +1,44 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
+//Brute force
+
+        // ArrayList<Integer> ls = new ArrayList<>();
+        // for(int i=0;i<nums.length;i++){
+        //     if(ls.contains(nums[i])){
+        //         continue;
+        //     }
+        //     int count =0;
+        //     for(int j=0;j<nums.length;j++){
+        //         if(nums[i]==nums[j]){
+        //             count++;
+        //         }
+        //     }
+        //     int limit = nums.length / 3;
+        //     if(count > limit){
+        //         ls.add(nums[i]);
+        //     }
+        // }
+        // return ls;
+
+        //Better 
+
         ArrayList<Integer> ls = new ArrayList<>();
+        HashMap<Integer , Integer> mp = new HashMap<>();
         for(int i=0;i<nums.length;i++){
-            if(ls.contains(nums[i])){
-                continue;
+            if(mp.containsKey(nums[i])){
+                mp.put(nums[i] , mp.get(nums[i])+1);
             }
-            int count =0;
-            for(int j=0;j<nums.length;j++){
-                if(nums[i]==nums[j]){
-                    count++;
-                }
-            }
-            int limit = nums.length / 3;
-            if(count > limit){
-                ls.add(nums[i]);
+            else {
+                mp.put(nums[i] , 1);
             }
         }
-        return ls;
+        int limit = nums.length / 3;
+        for(int key : mp.keySet()){
+            if(mp.get(key) > limit){
+                ls.add(key);
+            }
+        }
+return ls;
         
     }
 }
