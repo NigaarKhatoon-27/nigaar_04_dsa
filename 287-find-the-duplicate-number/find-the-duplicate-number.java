43bf8e71c05fecc6d085c1bfef3a2b1d49396e06
NIGAAR KHATOON <khatoonnigaar953@gmail.com>
2026-06-19 22:58:45 +0530

    Time: 20 ms (35.36%) | Memory: 91.9 MB (13.58%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/find-the-duplicate-number.java
@@ -18,18 +18,42 @@ class Solution {
 
 
         //Better approach using hashing
-        int repeating = -1;
-        int[] hash = new int[nums.length];
-        for(int i=0;i<nums.length;i++){
-            hash[nums[i]] += 1;
-        }
-        for(int i=1;i<hash.length;i++){
-            if(hash[i] >=2) {
-                repeating = i;
-            }
-        }
-        return repeating;
+        // int repeating = -1;
+        // int[] hash = new int[nums.length];
+        // for(int i=0;i<nums.length;i++){
+        //     hash[nums[i]] += 1;
+        // }
+        // for(int i=1;i<hash.length;i++){
+        //     if(hash[i] >=2) {
+        //         repeating = i;
+        //     }
+        // }
+        // return repeating;
 
+        //Best or optimal approach
+        // int n = nums.length -1; 
+        // int sumN = (n * (n+1))/2;
+        
+        // int s = 0;
+        
+        // for(int i=0;i<nums.length;i++){
+        //     s += nums[i];
+            
+        //     }
+        //     int repeating = s-sumN;
+        //     return repeating;
+
+//Using HashSet
+HashSet<Integer> st = new HashSet<>();
+for(int i=0;i<nums.length;i++){
+    if(st.contains(nums[i])){
+        return nums[i];
+    }
+    else {
+        st.add(nums[i]);
+    }
+}
+return -1;
         
     }
 }
