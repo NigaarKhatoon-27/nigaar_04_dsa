5ada3751839ce587debca5a4b686c939cdb877ef
NIGAAR KHATOON <khatoonnigaar953@gmail.com>
2026-06-20 22:58:58 +0530

    Time: 30 ms (77.85%) | Memory: 95.7 MB (39.92%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -1,72 +1,78 @@
 class Solution {
-    // public boolean linearSearch(int[] nums , int target){
+    // public boolean linearSearch(int[] nums , int n){
     //     for(int i=0;i<nums.length;i++){
-    //         if(nums[i]==target) return true;
+    //         if(nums[i]==n){
+    //             return true;
+    //         }
             
     //     }
-    //  return false;
+    //     return false;
     // }
+    
     public int longestConsecutive(int[] nums) {
-    //     if(nums.length==0) return 0;
     //     //Brute force
-    //     int longest = Integer.MIN_VALUE;
-    //     int count =1 ;
-    //     for(int i =0;i<nums.length;i++){
+    //     int longest =0;
+       
+    //     for(int i=0;i<nums.length;i++){
     //         int current = nums[i];
-    //         count =1;
-    //         while(linearSearch(nums ,current + 1)){
+    //         int count =1;
+    //         while(linearSearch(nums , current+1)){
     //             current++;
     //             count++;
     //         }
     //         longest = Math.max(longest , count);
+
+
+
     //     }
     //     return longest;
-        
 
-        //Better approach using sorting
-        // if(nums.length == 0) return 0;
-        // Arrays.sort(nums);
-        // int longest = 1;
-        // int count = 1;
-        // for(int i=1;i<nums.length;i++){
-        //     if(nums[i]== nums[i-1]){
-        //         continue;
-        //     }
-        //     else if(nums[i]==nums[i-1]+1){
-        //         count++;
-        //     }
-        //     else{
-        //         count = 1;
-        //     }
-        //     longest = Math.max(longest , count);
-        // }
-        // return longest;
+    //Better 
+//     if(nums.length == 0){
+//         return 0;
+//     }
+//     Arrays.sort(nums);
+//     int longest =1;
+//     int count =1;
+//     for(int i=1;i<nums.length;i++){
+//         if(nums[i]==nums[i-1]){
+//             continue;
+//         }
+//         else if(nums[i]==nums[i-1]+1){
+//             count++;
+//         }
 
-        //optimal approach
-        if(nums.length == 0) return 0;
-        HashSet<Integer> st = new HashSet<>();
-        for(int i=0;i<nums.length;i++){
-            st.add(nums[i]);
-        }
-       
-      
-        int longest = 1;
-        
+//         else{
+//             count =1;
+//         }
+//         longest = Math.max(longest , count);
+//     }
+// return longest;
 
-        for(int num : st){
-        if(!st.contains(num-1)){
+//Better
+HashSet<Integer> set = new HashSet<>();
+if(nums.length ==0) return 0;
+for(int i=0;i<nums.length;i++){
+    set.add(nums[i]);
+}
+
+int longest =1;
+
+for(int num : set ){
+    if(!set.contains(num-1)){
         int current = num;
-          int count = 1;
-        
-        while(st.contains(current+1)){
-            count++;
-            current++;
-        }
-        
-        longest = Math.max(count , longest);
-             }
-        }
-        return longest;
+        int count=1;
+    
+    while(set.contains(current + 1)){
+        current++;
+        count++;
+    }
+    longest = Math.max(longest , count);
+}
+}
+return longest;
 
+
+        
     }
 }
