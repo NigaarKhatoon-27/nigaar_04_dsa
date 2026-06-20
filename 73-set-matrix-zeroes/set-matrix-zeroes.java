31606769989c4a5ea0472e093bcf739af68a509a
NIGAAR KHATOON <khatoonnigaar953@gmail.com>
2026-06-20 23:10:01 +0530

    Time: 1 ms (98.41%) | Memory: 47.3 MB (90.65%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,79 +1,31 @@
-
-//Brute
-
-// class Solution {
-
-//     public void markRow(int[] row, int r) {
-//         row[r] = 1;
-//     }
-
-//     public void markCol(int[] col, int c) {
-//         col[c] = 1;
-//     }
-
-//     public void setZeroes(int[][] matrix) {
-
-//         int rows = matrix.length;
-//         int cols = matrix[0].length;
-
-//         int[] row = new int[rows];
-//         int[] col = new int[cols];
-
-//         // Mark rows and columns
-//         for (int i = 0; i < rows; i++) {
-//             for (int j = 0; j < cols; j++) {
-//                 if (matrix[i][j] == 0) {
-//                     markRow(row, i);
-//                     markCol(col, j);
-//                 }
-//             }
-//         }
-
-//         // Set elements to 0
-//         for (int i = 0; i < rows; i++) {
-//             for (int j = 0; j < cols; j++) {
-//                 if (row[i] == 1 || col[j] == 1) {
-//                     matrix[i][j] = 0;
-//                 }
-//             }
-//         }
-//     }
-// }
-
-
-//Better
-
 class Solution {
-
-   
-
- 
-
+    public void markRow(int[] row , int r){
+        row[r]=1;
+    }
+    public void markCol(int[] col , int s){
+        col[s]=1;
+    }
     public void setZeroes(int[][] matrix) {
-
         int rows = matrix.length;
         int cols = matrix[0].length;
-
         int[] row = new int[rows];
         int[] col = new int[cols];
-
-        // Mark rows and columns
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (matrix[i][j] == 0) {
-                    row[i]=1;
-                    col[j]=1;
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                if(matrix[i][j]==0){
+                    markRow(row , i);
+                    markCol(col , j);
                 }
             }
         }
-
-        // Set elements to 0
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (row[i] == 1 || col[j] == 1) {
-                    matrix[i][j] = 0;
+            for(int i=0;i<rows;i++){
+                for(int j=0;j<cols;j++){
+                    if(row[i]==1 || col[j]==1 ){
+                        matrix[i][j]=0;
+                    }
                 }
+
             }
         }
+        
     }
-}
