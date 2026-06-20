249c84a246e83c545b44f2ef19b42c66c0786d37
NIGAAR KHATOON <khatoonnigaar953@gmail.com>
2026-06-20 23:11:06 +0530

    Time: 1 ms (98.41%) | Memory: 47.7 MB (25.10%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,10 +1,33 @@
 class Solution {
-    public void markRow(int[] row , int r){
-        row[r]=1;
-    }
-    public void markCol(int[] col , int s){
-        col[s]=1;
-    }
+    // public void markRow(int[] row , int r){
+    //     row[r]=1;
+    // }
+    // public void markCol(int[] col , int s){
+    //     col[s]=1;
+    // }
+    // public void setZeroes(int[][] matrix) {
+    //     int rows = matrix.length;
+    //     int cols = matrix[0].length;
+    //     int[] row = new int[rows];
+    //     int[] col = new int[cols];
+    //     for(int i=0;i<rows;i++){
+    //         for(int j=0;j<cols;j++){
+    //             if(matrix[i][j]==0){
+    //                 markRow(row , i);
+    //                 markCol(col , j);
+    //             }
+    //         }
+    //     }
+    //         for(int i=0;i<rows;i++){
+    //             for(int j=0;j<cols;j++){
+    //                 if(row[i]==1 || col[j]==1 ){
+    //                     matrix[i][j]=0;
+    //                 }
+    //             }
+
+    //         }
+
+
     public void setZeroes(int[][] matrix) {
         int rows = matrix.length;
         int cols = matrix[0].length;
@@ -13,8 +36,8 @@ class Solution {
         for(int i=0;i<rows;i++){
             for(int j=0;j<cols;j++){
                 if(matrix[i][j]==0){
-                    markRow(row , i);
-                    markCol(col , j);
+                   row[i]=1;
+                   col[j]=1;
                 }
             }
         }
@@ -26,6 +49,8 @@ class Solution {
                 }
 
             }
+
+
         }
         
     }
