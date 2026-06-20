7f25a3f0664a5637e435b3eaffaddb775fbcbfdd
NIGAAR KHATOON <khatoonnigaar953@gmail.com>
2026-06-20 13:12:56 +0530

    Time: 2 ms (91.87%) | Memory: 47.8 MB (37.62%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,22 +1,14 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        //Brute force
-        int[] temp = new int[nums.length];
-       int k=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i] != 0){
-                temp[k] = nums[i];
-                k++;
+        int j = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j++;
             }
         }
-        for(int i=0;i<temp.length;i++){
-            nums[i]=temp[i];
-        }
-        int non_zero = temp.length;
-        for(int j =non_zero;j<nums.length;j++){
-            nums[j]=0;
-        }
-
-        
     }
 }
