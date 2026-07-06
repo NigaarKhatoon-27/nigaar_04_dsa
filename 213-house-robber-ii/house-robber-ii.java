f24d49107ac6edf03e667a5591bf59a5b0be0efb
NIGAAR KHATOON <khatoonnigaar953@gmail.com>
2026-07-07 03:01:15 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (40.83%) - LeetSync

---
+++ 213-house-robber-ii/house-robber-ii.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+         if (n == 1)
+            return nums[0];
+        if (n == 2)
+            return Math.max(nums[0], nums[1]);
+        return Math.max(solve(nums , 0 , n-2) , solve(nums , 1 , n-1));
+ }
+ public int solve(int[] nums , int start , int end){
+    int len =  end -start +1;
+    int[] dp = new int[len];
+    
+    dp[0] = nums[start];
+    dp[1] = Math.max(nums[start] , nums[start+1]);
+    for(int i=2;i<len;i++){
+        int pick = nums[start+i]+ dp[i-2];
+        int notpick = dp[i-1];
+        dp[i]=Math.max(pick , notpick);
+    }
+    return dp[len-1];
+ }
+}
