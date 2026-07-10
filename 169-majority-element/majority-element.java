f7954aeef33864d005e52e220eafe37ea80215ea
NIGAAR KHATOON <khatoonnigaar953@gmail.com>
2026-07-10 22:17:18 +0530

    Time: 15 ms (23.53%) | Memory: 52.4 MB (94.85%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,19 +1,38 @@
 class Solution {
     public int majorityElement(int[] nums) {
     //BRUTE FORCE
-    if(nums.length == 1) return nums[0];
-    int majority = nums.length / 2;
-    for(int i=0;i<nums.length;i++){
-        int count =1;
-        for(int j=i+1;j<nums.length;j++){
-            if(nums[i]==nums[j]){
-                count++;
-            }
-            if(count > majority) return nums[i];
-        }
+    // if(nums.length == 1) return nums[0];
+    // int majority = nums.length / 2;
+    // for(int i=0;i<nums.length;i++){
+    //     int count =1;
+    //     for(int j=i+1;j<nums.length;j++){
+    //         if(nums[i]==nums[j]){
+    //             count++;
+    //         }
+    //         if(count > majority) return nums[i];
+    //     }
     
+    // }
+    // return -1;
+
+    //Hashmapp
+    HashMap<Integer , Integer> map = new HashMap<>();
+    for(int i=0;i<nums.length;i++){
+        if(map.containsKey(nums[i])){
+            map.put(nums[i] , map.get(nums[i])+1);
+        }
+        else{
+            map.put(nums[i] , 1);
+        }
+    }
+    int majority = nums.length / 2;
+    for(int key : map.keySet()){
+        if(map.get(key) > majority){
+            return key;
+        }
     }
     return -1;
+
         
     }
 }
