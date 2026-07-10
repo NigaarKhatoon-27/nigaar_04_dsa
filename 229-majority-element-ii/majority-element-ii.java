af6d0feeb1221e7c7fae56ed095f23fbb1978d37
NIGAAR KHATOON <khatoonnigaar953@gmail.com>
2026-07-10 22:55:59 +0530

    Time: 3 ms (72.19%) | Memory: 52.7 MB (40.23%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -22,23 +22,62 @@ class Solution {
         // return list;
 
         //Hashmap
-        HashMap<Integer , Integer> map = new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            if(map.containsKey(nums[i])){
-                map.put(nums[i] , map.get(nums[i])+1);
-            }
-            else{
-                map.put(nums[i] , 1);
-            }
-        }
-        ArrayList<Integer> list = new ArrayList<>();
-        int limit = nums.length / 3;
-        for(int key : map.keySet()){
-           if(map.get(key) > limit){
-list.add(key);
-           }
-        }
-        return list;
+//         HashMap<Integer , Integer> map = new HashMap<>();
+//         for(int i=0;i<nums.length;i++){
+//             if(map.containsKey(nums[i])){
+//                 map.put(nums[i] , map.get(nums[i])+1);
+//             }
+//             else{
+//                 map.put(nums[i] , 1);
+//             }
+//         }
+//         ArrayList<Integer> list = new ArrayList<>();
+//         int limit = nums.length / 3;
+//         for(int key : map.keySet()){
+//            if(map.get(key) > limit){
+// list.add(key);
+//            }
+//         }
+//         return list;
+
+//Moore voting algorithm
+int count1=0;
+int count2 =0;
+int ele1= Integer.MIN_VALUE;
+int ele2 = Integer.MIN_VALUE;
+ArrayList<Integer> list = new ArrayList<>();
+for(int i=0;i<nums.length;i++){
+    if(count1 == 0 && ele2 != nums[i]){
+        count1++;
+        ele1 = nums[i];
+    }
+    else if(count2 == 0 && ele1 != nums[i]){
+        count2++;
+        ele2 = nums[i];
+    }
+    else if(ele1 == nums[i]){
+        count1++;
+    }
+    else if(ele2 == nums[i]){
+        count2++;
+    }
+    else{
+        count1--;
+        count2--;
+    }
+}
+count1=0;
+count2=0;
+for(int num : nums){
+    if(num == ele1) count1++;
+    if(num == ele2) count2++;
+}
+int limit = nums.length / 3;
+if(count1 > limit) list.add(ele1);
+if(count2 > limit) list.add(ele2);
+return list;
+
+
         
     }
 }
