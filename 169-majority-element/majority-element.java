681312f6c0533cf42d14363da976e172cfe808b7
NIGAAR KHATOON <khatoonnigaar953@gmail.com>
2026-07-10 22:20:20 +0530

    Time: 2 ms (70.26%) | Memory: 55.9 MB (6.57%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -16,22 +16,38 @@ class Solution {
     // return -1;
 
     //Hashmapp
-    HashMap<Integer , Integer> map = new HashMap<>();
-    for(int i=0;i<nums.length;i++){
-        if(map.containsKey(nums[i])){
-            map.put(nums[i] , map.get(nums[i])+1);
+    // HashMap<Integer , Integer> map = new HashMap<>();
+    // for(int i=0;i<nums.length;i++){
+    //     if(map.containsKey(nums[i])){
+    //         map.put(nums[i] , map.get(nums[i])+1);
+    //     }
+    //     else{
+    //         map.put(nums[i] , 1);
+    //     }
+    // }
+    // int majority = nums.length / 2;
+    // for(int key : map.keySet()){
+    //     if(map.get(key) > majority){
+    //         return key;
+    //     }
+    // }
+    // return -1;
+
+    //Boyre Moore algo;
+    int count =0;
+    int candidate = 0;
+    for(int num : nums){
+        if(count==0){
+            candidate = num;
+        }
+        if(candidate == num){
+            count++;
         }
         else{
-            map.put(nums[i] , 1);
+            count--;
         }
     }
-    int majority = nums.length / 2;
-    for(int key : map.keySet()){
-        if(map.get(key) > majority){
-            return key;
-        }
-    }
-    return -1;
+    return candidate;
 
         
     }
