c998f2a42a64e23ff4edf8b7af00c3e2de53e60c
NIGAAR KHATOON <khatoonnigaar953@gmail.com>
2026-07-14 00:54:15 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (52.16%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,7 +1,20 @@
 class Solution {
+    public static int solve(int[] dp , int n){
+        if(n<=1 ) return n;
+        if(dp[n] != -1) return dp[n];
+        return dp[n] = solve(dp , n-1)+ solve(dp , n-2);
+    }
     public int fib(int n) {
-        if(n<=1) return n;
-        return fib(n-1) + fib(n-2);
+        //Recursion
+        // if(n<=1) return n;
+        // return fib(n-1) + fib(n-2);
+
+        //Memoization
+        int[] dp = new int[n+1];
+        for(int i=0;i<=n;i++){
+            dp[i] = -1;
+        }
+        return solve(dp , n);
         
     }
 }
