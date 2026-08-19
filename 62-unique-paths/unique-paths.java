7606cf13b27038e8de08f823f5600809672cb906
NIGAAR KHATOON <khatoonnigaar953@gmail.com>
2026-08-19 18:04:35 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (48.28%) - LeetSync

---
+++ 62-unique-paths/unique-paths.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+    //       if (m == 1 || n == 1) {
+    //         return 1;
+    //     }
+    //     int right = uniquePaths(m , n-1);
+    //     int down = uniquePaths(m-1 , n);
+    //     return right+down;
+    // }
+    int[][] dp = new int[m][n];
+    return solve(m-1,n-1,dp);
+    }
+    public int solve(int m , int n , int[][] dp){
+        if(m==0 || n==0) return 1;
+        if(dp[m][n] != 0){
+            return dp[m][n];
+        }
+        int right = solve(m,n-1,dp);
+        int down = solve(m-1 , n , dp);
+        return dp[m][n] = right + down;
+    }
+}
